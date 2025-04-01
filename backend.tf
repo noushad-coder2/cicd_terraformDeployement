d553bb884f4f6959bd932ab27d619a54d13638e1
Karuthadath <karuthadath@hpe.com>
2025-04-01 21:14:58 +0530

    added backend6

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformbucketfromnoushadk"
-    key            = "terraform/terraform.tfstate"
+    key            = "terraform22/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
     
