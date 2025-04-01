8ad13ed384f9ed901132510223cebe29d2a84f05
noushad-coder2 <140237834+noushad-coder2@users.noreply.github.com>
2025-04-01 21:38:39 +0530

    Update backend6.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
  required_version = ">= 1.10.0"
 
    backend "s3" {
-     bucket         = "terraformbucketfromnoushadk"
+     bucket         = "terraformbucketfromnoushadk2"
      key            = "terraform2/terraform.tfstate"
      region         = "us-east-1"
      use_lockfile   = true
