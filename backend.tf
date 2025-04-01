1255d74e22437c2b93fd355b29fe5cd7cad26e92
Karuthadath <karuthadath@hpe.com>
2025-04-01 21:00:50 +0530

    added backend4

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformbucketfromnoushadk"
-    key            = "/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform_backend" # Replace with your actual DynamoDB table name
