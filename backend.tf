0328dcbd9875850c86608f9b9a470bc694d42dc2
Karuthadath <karuthadath@hpe.com>
2025-04-01 20:58:58 +0530

    added backend3

--- backend.tf
+++ backend.tf
@@ -1,11 +1,9 @@
-terraform {
+terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "path/to/terraform.tfstate"
-    region         = "us-west-2"
+    bucket         = "terraformbucketfromnoushadk"
+    key            = "/terraform.tfstate"
+    region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "your-terraform-lock-table" # Replace with your actual DynamoDB table name
+    dynamodb_table = "terraform_backend" # Replace with your actual DynamoDB table name
   }
-}[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[terraform_backend[A[A[D[D[D[D[D[D[D[1[D[D[[[C[C[C[C[C[C[[C[1;6D[1;6[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C"/terraform_state[C[C[C[C[C[C["[D[D[D[D[D[[C[C[C[F[B[F[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C.tfstaterraformbucketfromnoushadk[B[B[B[B[D[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C"[B
-[B
-
+}
