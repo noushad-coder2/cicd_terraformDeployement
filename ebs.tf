49735f182e7c637642ec4042e022d4e04fe40dc4
Karuthadath <karuthadath@hpe.com>
2025-04-01 23:48:40 +0530

    ebs5

--- ebs.tf
+++ ebs.tf
@@ -2,7 +2,7 @@
 resource "aws_ebs_volume" "my_volume2" {
   availability_zone = "us-east-1a" # Change to match your EC2 instance AZ
   size              = 20  # Size in GB
-  type              = "gp3" # Change to gp2, io1, io2, etc.
+  type              = "gp3" # Chang to gp2, io1, io2, etc.
   encrypted         = true
   tags = {
     Name = "MyEBSVolume"
